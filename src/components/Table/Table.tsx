233b1521622dbd0df86d80f0990dc69152522398
Yurii Shpakovych <yshpa@softserveinc.com>
2019-03-07 17:55:38 +0200

    added initial table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Table.css';
+
+class Table extends Component {
+    render() {
+      return (
+        <div className="Table">
+            Table
+        </div>
+      );
+    }
+  }
+  
+  export default Table;
